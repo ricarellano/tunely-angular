519098f3ce8799b846c372b5dcfb6462783bad0c
ricarellano <rarellanob@gmail.com>
2017-01-05 15:08:53 -0800

    almost finish sprint 1

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,3 +3,35 @@
  * This is your main angular file. Edit as you see fit.
  *
  */
+
+angular
+  .module('tunely', [])
+  .controller('AlbumsIndexController', AlbumsIndexController);
+  // ^ the first argument is a string naming the controller,
+  // the second argument is a function that defines the capacities
+  // of the controller.
+
+function AlbumsIndexController () {
+  var vm = this;
+  vm.newAlbum = {};
+
+  vm.newAlbum = {
+      name: 'Viva Hate',
+      artistName: 'Morrissey'
+  };
+
+  vm.albums = [
+    {
+      name: 'Coming Home',
+      artistName: 'Leon Bridges'
+    },
+    {
+      name: 'Are We There',
+      artistName: 'Sharon Van Etten'
+    },
+    {
+      name: 'The Queen is Dead',
+      artistName: 'The Smiths'
+    }
+  ];
+}
